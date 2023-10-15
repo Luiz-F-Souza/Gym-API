fc16a746d2572e08edc887fa2400ad452a245687
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-10-15 15:45:34 -0300

    feat: adiciona métodos findById, FindManyByUserId, countByUserId e save

--- src/repositories/@interface/checkin-interface.ts
+++ src/repositories/@interface/checkin-interface.ts
@@ -5,5 +5,13 @@ export interface CheckinRepositoryInterface {
 
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn | null>
 
+  findById(checkinId: string): Promise<CheckIn | null>
+
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+
+  countByUserId(userId: string): Promise<number>
+
+  save(checkin: CheckIn): Promise<CheckIn>
 }
