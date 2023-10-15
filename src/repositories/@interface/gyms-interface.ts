0e60367951c1c236e911e6f085c30ad2dd2439fe
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-10-15 15:46:08 -0300

    feat: adiciona métodos searchMany e findManyNear

--- src/repositories/@interface/gyms-interface.ts
+++ src/repositories/@interface/gyms-interface.ts
@@ -5,4 +5,6 @@ export interface GymsRepositoryInterface {
 
   create(data: Prisma.GymCreateInput): Promise<Gym>
   findById(id: string): Promise<Gym | null>
+  searchMany(query: string, page: number): Promise<Gym[]>
+  findManyNear(latitude: number, longitude: number): Promise<Gym[]>
 }
