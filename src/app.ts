afea6556aa7deac38eb6b2b547e7c0e99a8524a0
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-10-15 16:13:13 -0300

    feat: registra o fastifyCookie plugin e adiciona informações para o refreshToken e tempo de expiração do JWT

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,25 @@ import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { fastifyJwt } from '@fastify/jwt'
-
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
 
 
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m'
+  }
 })
+app.register(fastifyCookie)
 
 app.register(appRoutes)
+
 
 // Tratativa global, em caso de não termos tratado com return dentro de alguma camada 
 app.setErrorHandler((err, _request, reply) => {
