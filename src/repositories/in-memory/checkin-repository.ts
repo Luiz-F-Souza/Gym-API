a959f0de622180870770efcb521792303c75a609
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-10-15 15:46:41 -0300

    feat: implementa  métodos findById, FindManyByUserId, countByUserId e save

--- src/repositories/in-memory/checkin-repository.ts
+++ src/repositories/in-memory/checkin-repository.ts
@@ -25,7 +25,6 @@ export class InMemoryCheckinRepository implements CheckinRepositoryInterface {
 
   async findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null> {
 
-
     const checkinOnSameDate = this.fakeDataBase.find(checkin => {
 
       const isSameId = checkin.user_id === userId
@@ -34,11 +33,44 @@ export class InMemoryCheckinRepository implements CheckinRepositoryInterface {
       const isSameDate = dayjs(date).isSame(checkin.created_at, 'day')
       if (!isSameDate) return false
       else return true
-      
+
     })
 
     if (!checkinOnSameDate) return null
 
     return checkinOnSameDate
   }
+
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    const checkins = this.fakeDataBase.filter(checkin => checkin.user_id === userId)
+
+    const checkinsInSpecificPage = checkins.slice((page - 1) * 20, page * 20)
+
+    return checkinsInSpecificPage
+  }
+
+  async countByUserId(userId: string): Promise<number> {
+    const checkins = this.fakeDataBase.filter(checkin => checkin.user_id === userId)
+
+    const checkinsQuantity = checkins.length
+    return checkinsQuantity
+  }
+
+  async findById(checkinId: string): Promise<CheckIn | null> {
+
+    const checkin = this.fakeDataBase.find(checkin => checkin.id === checkinId)
+
+    if (!checkin) return null
+
+    return checkin
+  }
+
+  async save(checkin: CheckIn): Promise<CheckIn> {
+    const checkinIndex = this.fakeDataBase.findIndex(dbCheckin => dbCheckin.id === checkin.id)
+
+    if (checkinIndex > -1) this.fakeDataBase[checkinIndex] = checkin
+
+    return checkin
+  }
+
 }
