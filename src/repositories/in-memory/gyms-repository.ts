7310ee991e0a837ef8f800288f8d64ccd643d3de
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-10-15 15:47:17 -0300

    feat: implementa  métodos searchMany e findManyNear

--- src/repositories/in-memory/gyms-repository.ts
+++ src/repositories/in-memory/gyms-repository.ts
@@ -2,6 +2,8 @@ import { Decimal } from "@prisma/client/runtime/library";
 import { GymsRepositoryInterface } from "../@interface/gyms-interface";
 import { Gym, Prisma } from "@prisma/client";
 import { randomUUID } from "crypto";
+import { normalizingStrings } from "@src/utils/normalizingStrings";
+import { getDistanceBetweenCoordinates } from "@src/utils/get-distance-between-coordinates";
 
 
 export class InMemoryGymsRepository implements GymsRepositoryInterface {
@@ -32,5 +34,43 @@ export class InMemoryGymsRepository implements GymsRepositoryInterface {
     this.fakeDataBase.push(gym)
 
     return gym
+  }
+
+
+  async searchMany(query: string, page: number): Promise<Gym[]> {
+
+    const gyms = this.fakeDataBase.filter(gym => {
+
+      // const normalizedTitle = normalizingStrings(gym.title)
+      const doesTitleIncludesQuery = gym.title.toLowerCase().includes(query)
+      if (doesTitleIncludesQuery) return true
+
+      const normalizedDescription = normalizingStrings(gym.description ?? '')
+      const doesDescriptionIncludesQuery = normalizedDescription.includes(query)
+
+      if (doesDescriptionIncludesQuery) return true
+
+      return false
+    })
+
+    const paginatedGyms = gyms.slice((page - 1) * 20, page * 20)
+
+    return paginatedGyms
+  }
+
+
+  async findManyNear(userLtitude: number, userLongitude: number): Promise<Gym[]> {
+
+    const gyms = this.fakeDataBase.filter(gym => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: Number(gym.latitude), longitude: Number(gym.longitude) },
+        { latitude: userLtitude, longitude: userLongitude }
+      )
+
+      if (distance < 10) return true
+      return false
+    })
+
+    return gyms
   }
 }
